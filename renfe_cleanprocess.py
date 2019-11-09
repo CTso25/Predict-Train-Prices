73a0b6b216e3adf978efdbd0e07607734b44a091
Hannah <stoik.h@husky.neu.edu>
2019-11-09 18:34:03 -0500

    update

--- renfe_cleanprocess.py
+++ renfe_cleanprocess.py
@@ -17,7 +17,8 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn import preprocessing 
+from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
 
 renfedata = pd.read_csv("input/renfe.csv")
 
@@ -74,7 +75,6 @@ renfedata['destinationpop'] = renfedata.apply(destpop, axis = 1)
 #Madrid to Ponferrada
 
 #this is a  test 
-
 
 
 
