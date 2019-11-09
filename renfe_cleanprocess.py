23aa015b4456044dc2673e086dadfa601445d3a5
Ctso25 <c.tsoness@gmail.com>
2019-11-09 15:08:36 -0500

    update

--- renfe_cleanprocess.py
+++ renfe_cleanprocess.py
@@ -19,7 +19,7 @@ from sklearn.metrics import recall_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import preprocessing 
 
-renfedata = pd.read_csv("C:\\Users\\Chris\\Desktop\\renfe.csv")
+renfedata = pd.read_csv("input/renfe.csv")
 
 #add in population
 
@@ -42,38 +42,89 @@ renfedata['destinationpop'] = 0
 def originpop(data):
     if data['origin'] == 'MADRID':
         return 6550000
-    if data['origin'] == 'BARCELONA':
+    elif data['origin'] == 'BARCELONA':
         return 5515000
-    if data['origin'] == 'SEVILLA':
+    elif data['origin'] == 'SEVILLA':
         return 1945000
-    if data['origin'] == 'VALENCIA':
+    elif data['origin'] == 'VALENCIA':
         return 2531000
-    if data['origin'] == 'PONFERRADA':
+    elif data['origin'] == 'PONFERRADA':
         return 65239
 
 def destpop(data):
      if data['destination'] == 'MADRID':
          return 6550000
-     if data['destination'] == 'BARCELONA':
+     elif data['destination'] == 'BARCELONA':
         return 5515000
-     if data['destination'] == 'SEVILLA':
+     elif data['destination'] == 'SEVILLA':
         return 1945000
-     if data['destination'] == 'VALENCIA':
+     elif data['destination'] == 'VALENCIA':
         return 2531000
-     if data['destination'] == 'PONFERRADA':
+     elif data['destination'] == 'PONFERRADA':
         return 65239   
 
 renfedata['originpop'] = renfedata.apply(originpop, axis = 1)
 renfedata['destinationpop'] = renfedata.apply(destpop, axis = 1)
 
-#Euclidian Distance
-#based on driving distance
-#Madrid to Barcelona
-#Madrid to Sevilla
-#Madrid to Valencia
-#Madrid to Ponferrada
+#distances taken from here: https://www.trenes.com/
 
-#this is a  test 
+#505 km from Madrid to Barcelona
+#390 km from Madrid to Sevilla
+#302 km from Madrid to Valencia
+#338 km from Madrid to Ponferrada
+#829 km from Barcelona to Sevilla
+#303 km from Barcelona to Valencia
+#737 km from Barcelona to Ponferrada
+#539 km from Sevilla to Valencia
+#728 km from Sevilla to Ponferrada
+#641 km from Valencia to Ponferrada
+
+#helper function for distances between cities
+
+def distmeasure(data):
+    if data['destination'] == 'MADRID' and data['origin'] == 'BARCELONA':
+        return 505
+    elif data['destination'] == 'BARCELONA' and data['origin'] == 'MADRID':
+        return 505
+    elif data['destination'] == 'MADRID' and data['origin'] == 'SEVILLA':  
+        return 390
+    elif data['destination'] == 'SEVILLA' and data['origin'] == 'MADRID':
+        return 390
+    elif data['destination'] == 'MADRID' and data['origin'] == 'VALENCIA':  
+        return 302
+    elif data['destination'] == 'VALENCIA' and data['origin'] == 'MADRID':
+        return 302
+    elif data['destination'] == 'MADRID' and data['origin'] == 'PONFERRADA':  
+        return 338
+    elif data['destination'] == 'PONFERRADA' and data['origin'] == 'MADRID':
+        return 338
+    elif data['destination'] == 'BARCELONA' and data['origin'] == 'SEVILLA':  
+        return 829
+    elif data['destination'] == 'SEVILLA' and data['origin'] == 'BARCELONA':
+        return 829    
+    elif data['destination'] == 'BARCELONA' and data['origin'] == 'VALENCIA':  
+        return 303
+    elif data['destination'] == 'VALENCIA' and data['origin'] == 'BARCELONA':
+        return 303    
+    elif data['destination'] == 'BARCELONA' and data['origin'] == 'PONFERRADA':  
+        return 737
+    elif data['destination'] == 'PONFERRADA' and data['origin'] == 'BARCELONA':
+        return 737  
+    elif data['destination'] == 'SEVILLA' and data['origin'] == 'VALENCIA':  
+        return 539
+    elif data['destination'] == 'VALENCIA' and data['origin'] == 'SEVILLA':
+        return 539
+    elif data['destination'] == 'SEVILLA' and data['origin'] == 'PONFERRADA':
+        return 728
+    elif data['destination'] == 'PONFERRADA' and data['origin'] == 'SEVILLA':
+        return 728
+    elif data['destination'] == 'VALENCIA' and data['origin'] == 'PONFERRADA':
+        return 641
+    elif data['destination'] == 'PONFERRADA' and data['origin'] == 'VALENCIA':
+        return 641
+        
+renfedata['distance'] = renfedata.apply(distmeasure, axis = 1)
+
 
 
 
