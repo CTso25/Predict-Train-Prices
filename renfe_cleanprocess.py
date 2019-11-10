b4cade4a9264d29d9f5da1f6cee753547bd82ae3
Hannah <stoik.h@husky.neu.edu>
2019-11-09 19:03:28 -0500

    added ohe

--- renfe_cleanprocess.py
+++ renfe_cleanprocess.py
@@ -18,7 +18,8 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn import preprocessing 
+from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
 
 renfedata = pd.read_csv("input/renfe.csv")
 
