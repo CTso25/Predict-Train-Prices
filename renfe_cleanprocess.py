9d1f8590c4bd4d347217ca19fe38e84d8ceb2a4b
Hannah <stoik.h@husky.neu.edu>
2019-11-16 12:25:34 -0500

    cleaned data

--- renfe_cleanprocess.py
+++ renfe_cleanprocess.py
@@ -85,6 +85,82 @@ def destpop(data):
 renfedata['originpop'] = renfedata.apply(originpop, axis=1)
 renfedata['destinationpop'] = renfedata.apply(destpop, axis=1)
 
+# Read in holidays.csv to dataframe
+holidays_df = pd.read_csv('datasets/holidays.csv')
+
+# Create dictionary to store cities as hash keys
+holidays_dict = {
+    "MADRID": [],
+    "BARCELONA": [],
+    "VALENCIA": [],
+    "SEVILLA": [],
+    "PONFERRADA": []
+}
+
+# Add all holiday dates to dictionary for respective city
+for index, row in holidays_df.iterrows():
+    holidays_dict[holidays_df['rel_city'][index]].append(pd.to_datetime(holidays_df['date'][index]))
+
+# Helper function returns number of days away until next holiday/festival based on given date
+def daysToHoliday(items_mad, items_other, ticket_date):
+    nearest_mad_hol_date = min(items_mad, key=lambda x: (x.date() - ticket_date.date()).days if (x.date() - ticket_date.date()).days >= 0 else float('inf'))
+    nearest_otr_hol_date = min(items_other, key=lambda x: (x.date() - ticket_date.date()).days if (x.date() - ticket_date.date()).days >= 0 else float('inf'))
+
+    # Check to see if both cities have at least one holiday date in the future
+    # If not, only return the maximum date between the both i.e -  Madrid's next holiday
+    if nearest_mad_hol_date.date() >= ticket_date.date() and nearest_otr_hol_date.date() >= ticket_date.date():
+        nearest_date = min(nearest_mad_hol_date, nearest_otr_hol_date)
+    else:
+        nearest_date = max(nearest_mad_hol_date, nearest_otr_hol_date)
+
+    return (nearest_date.date() - ticket_date.date()).days
+
+# Helper function returns number of days that have passed since last holiday/festival based on given date
+def daysFromHoliday(items_mad, items_other, ticket_date):
+    nearest_mad_hol_date = max(items_mad, key=lambda x: (x.date() - ticket_date.date()).days if (x.date() - ticket_date.date()).days <= 0 else float('-inf'))
+    nearest_otr_hol_date = max(items_other, key=lambda x: (x.date() - ticket_date.date()).days if (x.date() - ticket_date.date()).days <= 0 else float('-inf'))
+
+    # Check to see if both cities have at least one holiday date in the past
+    # If not, only return the maximum date between the both i.e -  Madrid's last holiday
+    if nearest_mad_hol_date.date() <= ticket_date.date() and nearest_otr_hol_date.date() <= ticket_date.date():
+        nearest_date = max(nearest_mad_hol_date, nearest_otr_hol_date)
+    else:
+        nearest_date = min(nearest_mad_hol_date, nearest_otr_hol_date)
+
+    return abs((nearest_date.date() - ticket_date.date()).days)
+
+# Function to compute days to holiday on full data frame
+def computeDaysToHoliday(data):
+    if data['origin'] == 'MADRID':
+        city_to_check = data['destination']
+    else:
+        city_to_check = data['origin']
+
+    # Extract list of holiday dates for MADRID and other CITY
+    madrid_dates = holidays_dict.get('MADRID')
+    local_city_dates = holidays_dict.get(city_to_check)
+
+    # Compute the minimum number of days away to nearest holiday for both and return the minimum of both
+    return daysToHoliday(madrid_dates, local_city_dates, pd.to_datetime(data['start_date']))
+
+# Function to compute days from holiday on full data frame
+def computeDaysFromHoliday(data):
+    if data['origin'] == 'MADRID':
+        city_to_check = data['destination']
+    else:
+        city_to_check = data['origin']
+
+    # Extract list of holiday dates for MADRID and other CITY
+    madrid_dates = holidays_dict.get('MADRID')
+    local_city_dates = holidays_dict.get(city_to_check)
+
+    # Compute the minimum number of days away to nearest holiday for both and return the minimum of both
+    return daysFromHoliday(madrid_dates, local_city_dates, pd.to_datetime(data['start_date']))
+
+# Apply and create two new columns in data frame
+renfedata['days_to_holiday'] = renfedata.apply(computeDaysToHoliday, axis=1)
+renfedata['days_from_holiday'] = renfedata.apply(computeDaysFromHoliday, axis=1)
+
 # distances taken from here: https://www.trenes.com/
 
 # 505 km from Madrid to Barcelona
@@ -183,4 +259,6 @@ renfedata['train_class'] = renfedata.apply(trainClasses, axis=1)
 
 renfedata = renfedata.dropna()
 
-renfedata = pd.get_dummies(renfedata, columns=['origin', 'destination', 'day_name', 'train_type', 'fare', 'train_class'])
+renfedata = pd.get_dummies(renfedata, columns=['origin', 'destination', 'dotw', 'train_type', 'fare', 'train_class'])
+
+renfedata.to_csv("input/cleaned_data.csv")
