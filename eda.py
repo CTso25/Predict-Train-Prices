35bdba5501856f1fabc56726067873d9fc60a4d3
Hannah <stoik.h@husky.neu.edu>
2019-11-18 17:24:55 -0500

    corr plot

--- eda.py
+++ eda.py
@@ -17,14 +17,20 @@ from sklearn.preprocessing import OneHotEncoder
 
 renfe_nccd = pd.read_csv("input/not_encoded_clean_data.csv")
 
+# is overnight
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['is_overnight'])
+plt.xlabel("is overnight")
+plt.xticks([0, 1], ['No', 'Yes'])
 plt.show()
 
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='is_overnight', y='price', data=renfe_nccd)
+plt.xlabel("is overnight")
+plt.xticks([0, 1], ['No', 'Yes'])
 plt.show()
 
+# train type
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['train_type'])
 plt.show()
@@ -33,6 +39,7 @@ fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='train_type', y='price', data=renfe_nccd)
 plt.show()
 
+# train class
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['train_class'])
 plt.show()
@@ -41,6 +48,7 @@ fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='train_class', y='price', data=renfe_nccd)
 plt.show()
 
+# fare
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['fare'])
 plt.show()
@@ -49,18 +57,20 @@ fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='fare', y='price', data=renfe_nccd)
 plt.show()
 
+# day of the week
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['dotw'])
 plt.xlabel("day of the week")
-plt.xticks([0, 1, 2, 3, 4, 5, 6], ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat'])
+plt.xticks([0, 1, 2, 3, 4, 5, 6], ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'])
 plt.show()
 
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='dotw', y='price', data=renfe_nccd)
 plt.xlabel("day of the week")
-plt.xticks([0, 1, 2, 3, 4, 5, 6], ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat'])
+plt.xticks([0, 1, 2, 3, 4, 5, 6], ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'])
 plt.show()
 
+# date
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['date'])
 plt.show()
@@ -69,6 +79,7 @@ fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='date', y='price', data=renfe_nccd)
 plt.show()
 
+# month
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['month'])
 plt.xticks([0, 1, 2, 3, 4, 5, 6], ['apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct'])
@@ -79,6 +90,7 @@ ax = sns.boxplot(x='month', y='price', data=renfe_nccd)
 plt.xticks([0, 1, 2, 3, 4, 5, 6], ['apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct'])
 plt.show()
 
+# days to holiday
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['days_to_holiday'])
 plt.show()
@@ -87,6 +99,7 @@ fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='days_to_holiday', y='price', data=renfe_nccd)
 plt.show()
 
+# days from holiday
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['days_from_holiday'])
 plt.show()
@@ -95,10 +108,16 @@ fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='days_from_holiday', y='price', data=renfe_nccd)
 plt.show()
 
+# days to trip
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.countplot(renfe_nccd['days_to_trip'])
 plt.show()
 
 fig,ax = plt.subplots(figsize=(20,6))
 ax = sns.boxplot(x='days_to_trip', y='price', data=renfe_nccd)
+plt.show()
+
+# correlation plot
+f,ax = plt.subplots(figsize=(20, 20))
+sns.heatmap(renfe_nccd, annot=False, cmap = "Blues", linewidths=.5, fmt='.2f', ax=ax)
 plt.show()
