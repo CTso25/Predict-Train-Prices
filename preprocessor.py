f4a59c4ad4db2e30c5a038c0c74491b1d168ece6
bicachu <bicajohn1@gmail.com>
2019-12-08 12:20:06 -0500

    added minMax scalar

--- preprocessor.py
+++ preprocessor.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 renfedata = pd.read_csv("input/cleaned_data.csv")
 
 # keeping sample data in here for easy processing/debugging --> to remove later
-renfedata = renfedata.sample(n=50000, random_state=1)
+renfedata = renfedata.sample(n=50000)
 
 renfedata = renfedata.drop(columns=['Unnamed: 0','start_date', 'end_date'], axis=1)
 
@@ -36,9 +36,13 @@ def prepare_data():
     X_test = X_test.drop(columns=['insert_date'], axis=1)
 
     # standardize X_train and X_test
-    std_scaler = preprocessing.StandardScaler().fit(X_train)
-    X_train_scaled = std_scaler.transform(X_train)
-    X_test_scaled = std_scaler.transform(X_test)
+    minMax_scaler = preprocessing.MinMaxScaler().fit(X_train)
+    X_train_scaled = minMax_scaler.transform(X_train)
+    X_test_scaled = minMax_scaler.transform(X_test)
+
+    # std_scaler = preprocessing.StandardScaler().fit(X_train)
+    # X_train_scaled = std_scaler.transform(X_train)
+    # X_test_scaled = std_scaler.transform(X_test)
 
     return X_train_scaled, X_test_scaled, y_train, y_test
 
