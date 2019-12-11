89375d4c2ed2c0de5e5a82d40432a66a3803b569
Hannah <stoik.h@husky.neu.edu>
2019-12-11 00:02:10 -0500

    updated

--- eda.py
+++ eda.py
@@ -18,46 +18,74 @@ from sklearn.preprocessing import OneHotEncoder
 renfe_nccd = pd.read_csv("input/not_encoded_clean_data.csv")
 renfe_clean = pd.read_csv("input/cleaned_data.csv")
 renfe_cleaned = renfe_clean.drop(columns = ['Unnamed: 0', 'insert_date', 'start_date', 'end_date'])
+
+# renfe_cleaned = renfe_cleaned.drop(renfe_cleaned.index[renfe_cleaned['train_class']=='Cama G. Clase'].values)
+# renfe_cleaned = renfe_cleaned.drop(renfe_cleaned.index[renfe_cleaned['fare']=='Individual Sleeper-Flexible'].values)
+# renfe_cleaned = renfe_cleaned.drop(renfe_cleaned.index[renfe_cleaned['fare']=='Adulto Ida'].values)
+# renfe_cleaned = renfe_cleaned.drop(renfe_cleaned.index[renfe_cleaned['origin']=='MADRID'].values)
+# renfe_cleaned = renfe_cleaned.drop(renfe_cleaned.index[renfe_cleaned['destination']=='MADRID'].values)
+
+# dfEdgeArtistMuseum = dfEdgeArtistMuseum.drop(dfEdgeArtistMuseum.index[dfEdgeArtistMuseum['museum_id']==dvalue].values)
+
 renfe_cleaned = renfe_cleaned[renfe_cleaned.columns.values].astype(float)
 
-# is overnight
-fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.countplot(renfe_nccd['is_overnight'])
-plt.xlabel("is overnight")
-plt.xticks([0, 1], ['No', 'Yes'])
-plt.show()
+# # is overnight
+# fig,ax = plt.subplots(figsize=(20,6))
+# ax = sns.countplot(renfe_nccd['is_overnight'])
+# plt.xlabel("is overnight")
+# plt.xticks([0, 1], ['No', 'Yes'])
+# plt.show()
+#
+# fig,ax = plt.subplots(figsize=(20,6))
+# ax = sns.boxplot(x='is_overnight', y='price', data=renfe_nccd)
+# plt.xlabel("is overnight")
+# plt.xticks([0, 1], ['No', 'Yes'])
+# plt.show()
+
+# # train type
+# fig,ax = plt.subplots(figsize=(20,6))
+# ax = sns.countplot(renfe_nccd['train_type'])
+# plt.show()
+
+# fig,ax = plt.subplots(figsize=(20,6))
+# ax = sns.boxplot(x='train_type', y='price', data=renfe_nccd)
+# plt.show()
+
+# # train class
+# fig,ax = plt.subplots(figsize=(20,6))
+# ax = sns.countplot(renfe_nccd['train_class'])
+# plt.show()
+
+class_list = renfe_nccd['train_class']
+y_list = renfe_nccd['price']
+x = []
+y = []
+for i in range(len(class_list)):
+    if class_list[i] != 'Cama G. Clase':
+        x.append(class_list[i])
+        y.append(y_list[i])
 
 fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.boxplot(x='is_overnight', y='price', data=renfe_nccd)
-plt.xlabel("is overnight")
-plt.xticks([0, 1], ['No', 'Yes'])
+ax = sns.boxplot(x=x, y=y)
 plt.show()
 
-# train type
-fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.countplot(renfe_nccd['train_type'])
-plt.show()
+
+# # fare
+# fig,ax = plt.subplots(figsize=(20,6))
+# ax = sns.countplot(renfe_nccd['fare'])
+# plt.show()
+
+fare_list = renfe_nccd['fare']
+y_list = renfe_nccd['price']
+x = []
+y = []
+for i in range(len(fare_list)):
+    if (fare_list[i] != 'Individual Sleeper-Flexible' and fare_list[i] != 'Adulto Ida'):
+        x.append(fare_list[i])
+        y.append(y_list[i])
 
 fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.boxplot(x='train_type', y='price', data=renfe_nccd)
-plt.show()
-
-# train class
-fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.countplot(renfe_nccd['train_class'])
-plt.show()
-
-fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.boxplot(x='train_class', y='price', data=renfe_nccd)
-plt.show()
-
-# fare
-fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.countplot(renfe_nccd['fare'])
-plt.show()
-
-fig,ax = plt.subplots(figsize=(20,6))
-ax = sns.boxplot(x='fare', y='price', data=renfe_nccd)
+ax = sns.boxplot(x=x, y=y)
 plt.show()
 
 # day of the week
